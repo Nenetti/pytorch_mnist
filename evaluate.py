97de21a756a2581f7f3806e8655167acd8c740a3
Nenetti <akinori1090@gmail.com>
2019-03-06 00:27:56 +0900

    引数datasetを追加, datasetのパスを修正

--- evaluate.py
+++ evaluate.py
@@ -1,87 +1,17 @@
 from __future__ import print_function
 import torch
-from torchvision import datasets, transforms
 import os
 from model import MnistModel
 import torch.utils.data
 import argparse
-from tqdm import tqdm
-from PIL import Image
-
-
-def get_dataset(path):
-    """Load dataset in local.
-
-    Load train and test dataset in local.
-
-    Args:
-        path (str): Local path of dataset
-
-    Returns:
-        torchvision.datasets: Train dataset
-        torchvision.datasets: Test dataset
-
-    Example::
-        get_dataset(./hoge)
-
-    """
-    train_dataset = datasets.MNIST(root=path, train=True, download=True,
-                                   transform=transforms.Compose([
-                                       transforms.ToTensor(),
-                                       transforms.Normalize((0.1307,), (0.3081,))
-                                   ]))
-
-    test_dataset = datasets.MNIST(root=path, train=False,
-                                  transform=transforms.Compose([
-                                      transforms.ToTensor(),
-                                      transforms.Normalize((0.1307,), (0.3081,))
-                                  ]))
-
-    return train_dataset, test_dataset
-
-
-def load_dataset(path):
-    """Load dataset in local.
-
-    Load test image in local.
-    Folder Tree
-
-           Path
-            ├─ Label 1
-            |   ├─ Image 1
-            |   ├─ Image 2
-            |   └─ Image 3
-            |
-            └─ Label 2
-                └─ Image 1
-
-    Args:
-        path (str): Directory path of dataset in Local
-
-    Returns:
-        List[(tensor.torch, int)]: dataset
-
-    Example::
-        get_dataset(./hoge)
-
-    """
-    datasets = []
-    labels = os.listdir(path)
-    for label in tqdm(labels, desc=" Label ", ascii=True):
-        files = os.listdir(path + "/" + label)
-        for file in tqdm(files, desc=" Data  ", ascii=True):
-            img = Image.open(path + "/" + label + "/" + file).convert("L")
-            torch_img = transforms.ToTensor()(img)
-            data = (torch_img, int(label))
-            datasets.append(data)
-    print()
-    return datasets
+import dataset
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--load_model', default="trained_model.pt", help="Load path of Trained model in local")
     parser.add_argument('--device', default="cuda", help='Save path of Trained model in local')
+    parser.add_argument('--dataset', default=os.path.dirname(os.path.abspath(__file__)), help='Load path of image dataset in local')
     args = parser.parse_args()
 
     device = torch.device("cuda" if (args.device == "cuda" and torch.cuda.is_available()) else "cpu")
@@ -101,7 +31,7 @@ def main():
 
     model.set_device(device)
 
-    test_dataset = load_dataset(os.environ["HOME"] + "/dataset/MNIST/test")
+    test_dataset = dataset.load_dataset(args.dataset+"/MNIST/test")
     # hoge, test_dataset = get_dataset(os.environ["HOME"] + "/dataset")
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=True)
     model.set_test_loader(test_loader)
